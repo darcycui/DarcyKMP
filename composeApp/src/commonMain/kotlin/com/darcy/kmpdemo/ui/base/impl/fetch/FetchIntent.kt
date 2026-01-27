8af1c641bab28fe88ddd984e2fb7777afcdbf8a6
Tom_docker <Tom_docker@jerry.com>
2026-01-27 11:30:45 +0800

    优化：添加注释

--- composeApp/src/commonMain/kotlin/com/darcy/kmpdemo/ui/base/impl/fetch/FetchIntent.kt
+++ composeApp/src/commonMain/kotlin/com/darcy/kmpdemo/ui/base/impl/fetch/FetchIntent.kt
@@ -4,6 +4,7 @@ import com.darcy.kmpdemo.ui.base.IIntent
 
 sealed class FetchIntent : IIntent {
     data object ActionLoadData : FetchIntent()
+
     data class RefreshByFetchData<T>(
         val result: T
     ) : FetchIntent()
--- composeApp/src/commonMain/kotlin/com/darcy/kmpdemo/ui/base/impl/paging/PagingIntent.kt
+++ composeApp/src/commonMain/kotlin/com/darcy/kmpdemo/ui/base/impl/paging/PagingIntent.kt
@@ -2,10 +2,11 @@ package com.darcy.kmpdemo.ui.base.impl.paging
 
 import com.darcy.kmpdemo.ui.base.IIntent
 
-sealed class PagingIntent: IIntent {
+sealed class PagingIntent : IIntent {
     data class ActionLoadNewPage(
         val pageNumber: Int,
     ) : PagingIntent()
+
     data class RefreshByLoadNewPage<R>(
         val pageNumber: Int,
         val response: R,
